b4bdcd39a7819b82943c51dff9a58509c4046fcf
zhiy.wang <beixide@163.com>
2021-06-25 12:48:44 +0800

    change logger name

--- core/src/main/java/zhan/wang/moon/encode/MD5Util.java
+++ core/src/main/java/zhan/wang/moon/encode/MD5Util.java
@@ -2,7 +2,13 @@ package zhan.wang.moon.encode;
 
 import zhan.wang.moon.exception.EarthBaseException;
 import zhan.wang.moon.exception.StatusCode;
+import zhan.wang.moon.log.MoonSeqLogger;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -19,14 +25,36 @@ public class MD5Util {
     }
 
     /**
-     * 获得MD5值
+     * 获得MD5值 BASE16编码
      * @param source 输入值
      * @return MD5字符串
      */
-    public static String getMD5(String source) {
+    public static String getStringMD5Base16(String source) {
         messageDigest.update(source.getBytes());
         byte[] digest = messageDigest.digest();
         return HexUtil.byte2hex(digest);
+    }
+
+    public static String getFileMD5StringBase16(File file) {
+        String FileMD5String = null;
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+            InputStream fis = new FileInputStream(file);
+            byte[] buffer = new byte[1024];
+            int length = -1;
+            while ((length = fis.read(buffer, 0, 1024)) > 0) {
+                messageDigest.update(buffer, 0, length);
+            }
+            fis.close();
+            FileMD5String = new BigInteger(1, messageDigest.digest()).toString(16);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        MoonSeqLogger.info("文件名：{}，MD5：{}，大小为：{}M", file.getAbsolutePath(), FileMD5String, file.length() / 1024 / 1024);
+//        System.out.println("文件名：" + file.getAbsolutePath() + "\tMD5" + MD5Str + "\t大小为" + file.length()/1024/1024 + "M");
+        return FileMD5String;
     }
 
 }
--- file/src/main/java/zhan/wang/moon/FileUtil.java
+++ file/src/main/java/zhan/wang/moon/FileUtil.java
@@ -1,17 +1,14 @@
 package zhan.wang.moon;
 
 
+import zhan.wang.moon.encode.MD5Util;
 import zhan.wang.moon.log.MoonSeqLogger;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.math.BigInteger;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class FileUtil {
 
@@ -31,9 +28,7 @@ public class FileUtil {
             try {
                 for (File file : sourcePath.listFiles()) {
                     if (file.isFile()) {
-                        if (!file.getName().contains("downloading")) {
-                            returnFiles.add(file);
-                        }
+                        returnFiles.add(file);
                     } else {
                         returnFiles.addAll(readFiles(file));
                     }
@@ -43,31 +38,6 @@ public class FileUtil {
             }
         }
         return returnFiles;
-    }
-
-    public String getFileMD5String(File file) {
-        String MD5Str = null;
-        try {
-            mMessageDigest = MessageDigest.getInstance("MD5");
-        } catch (NoSuchAlgorithmException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        try {
-            InputStream fis = new FileInputStream(file);
-            byte[] buffer = new byte[1024];
-            int length = -1;
-            while ((length = fis.read(buffer, 0, 1024)) > 0) {
-                mMessageDigest.update(buffer, 0, length);
-            }
-            fis.close();
-            MD5Str = new BigInteger(1, mMessageDigest.digest()).toString(16);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        MoonSeqLogger.info("文件名：{}，MD5：{}，大小为：{}M", file.getAbsolutePath(), MD5Str, file.length() / 1024 / 1024);
-//        System.out.println("文件名：" + file.getAbsolutePath() + "\tMD5" + MD5Str + "\t大小为" + file.length()/1024/1024 + "M");
-        return MD5Str;
     }
 
     public void deleteFileStr(String filePath) {
@@ -85,15 +55,8 @@ public class FileUtil {
      */
     public boolean compareFile(File oldFilePath, File newFilePath) {
 
-//        File oldFile = new File(oldFilePath);
-//        File newFile = new File(newFilePath);
-//        System.out.println(oldFile.getName());
-//        file.getName().substring(file.getName().lastIndexOf(".")+1)
-//        System.out.println(oldFile.getAbsoluteFile());
-//        System.out.println(oldFile.getAbsoluteFile().toString().substring(oldFile.getAbsoluteFile().toString().lastIndexOf(".")+1));
         //跳过不同的文件格式
         if (!oldFilePath.getName().substring(oldFilePath.getName().lastIndexOf(".") + 1).equals(oldFilePath.getName().substring(oldFilePath.getName().lastIndexOf(".") + 1))) {
-//            System.out.println("文件格式不同，跳过\t" + oldFilePath.getAbsolutePath() + "\t" + oldFilePath.getAbsolutePath());
             MoonSeqLogger.info("文件格式不同，跳过\t 旧文件为：{}，新文件为：{}", oldFilePath.getAbsolutePath(), newFilePath.getAbsolutePath());
             return false;
         }
@@ -101,8 +64,8 @@ public class FileUtil {
         if (oldFilePath.getName().length() != newFilePath.getName().length()) {
             String oldFileMD5 = null;
             String newFileMD5 = null;
-            oldFileMD5 = getFileMD5String(oldFilePath);
-            newFileMD5 = getFileMD5String(newFilePath);
+            oldFileMD5 = MD5Util.getFileMD5StringBase16(oldFilePath);
+            newFileMD5 = MD5Util.getFileMD5StringBase16(newFilePath);
 
             if (newFileMD5 == null || oldFileMD5 == null || !oldFileMD5.equals(newFileMD5)) {
                 return false;
@@ -113,14 +76,20 @@ public class FileUtil {
         return true;
     }
 
-    public void moveFile(File file,String destPath) {
-        File newFile = new File(destPath + File.separator + file.getName());
+    /**
+     * 文件转移
+     * @param fileName 文件名
+     * @param destPath
+     */
+    public void moveFile(String fileName,String destPath) {
+        File file = new File(fileName);
+        File newFile = new File(destPath + File.separator + fileName);
         if (file.isFile()) {
             file.renameTo(newFile);
         }else {
             File[] files = file.listFiles();
-            for (File tempFile:files  ) {
-                moveFile(tempFile, destPath);
+            for (File tempFile:files) {
+                moveFile(tempFile.getName(), destPath);
             }
         }
     }
